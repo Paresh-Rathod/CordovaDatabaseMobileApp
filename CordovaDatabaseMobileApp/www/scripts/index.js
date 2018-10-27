fce9bee0f9aa4bba020da46e62b015f2225c901e
Paresh-Rathod <pareshrathod@outlook.com>
2018-10-27 17:47:33 +0300

    Add files via upload

--- CordovaDatabaseMobileApp/www/scripts/index.js
+++ CordovaDatabaseMobileApp/www/scripts/index.js
@@ -1,30 +1,22 @@
-﻿// For an introduction to the Blank template, see the following documentation:
-// http://go.microsoft.com/fwlink/?LinkID=397704
-// To debug code on page load in cordova-simulate or on Android devices/emulators: launch your app, set breakpoints, 
-// and then run "window.location.reload()" in the JavaScript Console.
-(function () {
-    "use strict";
+﻿//Creating a datbase
+$(document).on("ready", function () {
+    databaseHandler.createDatabase();
 
-    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
+});
 
-    function onDeviceReady() {
-        // Handle the Cordova pause and resume events
-        document.addEventListener( 'pause', onPause.bind( this ), false );
-        document.addEventListener( 'resume', onResume.bind( this ), false );
-        
-        // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
-        var parentElement = document.getElementById('deviceready');
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-    };
+//Adding item in table
+function addProduct() {
+    var name = $("#txtName").val();
+    var quantity = $("#txtQuantity").val();
 
-    function onPause() {
-        // TODO: This application has been suspended. Save application state here.
-    };
-
-    function onResume() {
-        // TODO: This application has been reactivated. Restore application state here.
-    };
-} )();
+    if (!name) {
+        alert("Name is required");
+    } else {
+        var r = confirm("Name: " + name + "\n" + "Quantity: " + quantity);
+        if (r == true) {
+            productHandler.addProduct(name, quantity);
+            $("#txtName").val("");
+            $("#txtQuantity").val("");
+        }
+    }
+}
